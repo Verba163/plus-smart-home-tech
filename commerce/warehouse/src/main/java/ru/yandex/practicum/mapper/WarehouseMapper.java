837617813a1e2e6c61c6646abd9ddceba45a3b2a
DmitriiFomin <dmi7f0min@yandex.ru>
2025-08-30 13:57:00 +0300

    1. Исправить Баги 3 модулей shopping-store — исправить ; shopping-cart — Исправить изменение количества товаров; warehouse — исправить двойное переназначение ID.

--- commerce/warehouse/src/main/java/ru/yandex/practicum/mapper/WarehouseMapper.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/mapper/WarehouseMapper.java
@@ -9,11 +9,14 @@ import ru.yandex.practicum.warehouse.model.dto.NewProductInWarehouseRequest;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface WarehouseMapper {
 
+    @Mapping(target = "productId", source = "productId")
     @Mapping(target = "width", source = "dimension.width")
     @Mapping(target = "height", source = "dimension.height")
     @Mapping(target = "depth", source = "dimension.depth")
     @Mapping(target = "fragile", source = "fragile")
     @Mapping(target = "weight", source = "weight")
+    @Mapping(target = "quantity", ignore = true)
+    @Mapping(target = "reservedQuantity", ignore = true)
     Warehouse toWarehouse(NewProductInWarehouseRequest dto);
 
 }
